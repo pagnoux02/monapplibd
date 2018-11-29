b49575d020846ed32106ff0edfa27e18f9024c0c
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-11-29 13:50:28 +0100

    yo

--- app/src/main/java/com/example/fpagnoux/monapplibd/activity_classement.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/activity_classement.java
@@ -9,5 +9,6 @@ public class activity_classement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classement);
+        //yyy
     }
 }
