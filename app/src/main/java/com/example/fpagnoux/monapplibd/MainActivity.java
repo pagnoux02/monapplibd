9aa263752a5b07ed393b04295e8922050bd75c6d
francois pagnoux <francois.pagnoux@outlook.fr>
2018-11-29 13:30:47 +0200

    lundi

--- app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.fpagnoux.monapplibd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView img ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.rene_la_bd);
+
     }
+
+    public void click()
+    {
+        img = (ImageView)findViewById(R.id.imgjeu1);
+        img.setOnClickListener(new View.OnClickListener() {
+
+            @Override public void onClick(View v) {
+            //    Intent intent = new Intent("fgedfydfcgu.imagesethh.Main2Activity");
+            //    startActivity(intent);
+            }
+        });
+    }
+
+
 }
