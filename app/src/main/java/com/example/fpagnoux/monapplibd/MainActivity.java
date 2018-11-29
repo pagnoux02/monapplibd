f6b18189e27218adec299c91fa24a17a9badd314
francois pagnoux <francois.pagnoux@outlook.fr>
2018-11-29 16:15:24 +0200

    JE PUSH CHIEN DES BOIS e

--- app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
@@ -2,30 +2,38 @@ package com.example.fpagnoux.monapplibd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img ;
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rene_la_bd);
+        text = (TextView)findViewById(R.id.txtscore);
+
 
     }
 
-    public void click()
-    {
-        img = (ImageView)findViewById(R.id.imgjeu1);
-        img.setOnClickListener(new View.OnClickListener() {
+    //public void click()
+    //{
+     //   img = (ImageView)findViewById(R.id.imgjeu1);
+      //  img.setOnClickListener(new View.OnClickListener() {
 
-            @Override public void onClick(View v) {
-            //    Intent intent = new Intent("fgedfydfcgu.imagesethh.Main2Activity");
-            //    startActivity(intent);
-            }
-        });
-    }
+          //  @Override public void onClick(View v) {
+
+
+
+          //      text.setText("A vous de jouer  ...");
+         //   }
+        //});
+    //}
 
 
 }
--- app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
@@ -1,4 +1,8 @@
 package com.example.fpagnoux.monapplibd;
 
 public class reneActivity {
+
+
+
+
 }
