df0669fb14b6f50c0edb5bb6b3cd6b08a206ee4a
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-11-29 12:17:13 +0100

    yo

--- app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.fpagnoux.monapplibd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText saisie;
+    private String pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//bonjour
+
+        Button valider = this.findViewById(R.id.buttValider);
+        valider.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        saisie = this.findViewById(R.id.SaisiPseudo);
+        pseudo = saisie.getText().toString();
     }
 }
