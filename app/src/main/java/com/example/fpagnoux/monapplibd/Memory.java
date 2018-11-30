aeba4c25204dafe7dd7255af32b59c41c02effb0
Maël CLEMENT <maelclement.pro@gmail.com>
2018-11-30 16:16:22 +0100

    ...

---
+++ app/src/main/java/com/example/fpagnoux/monapplibd/Memory.java
@@ -0,0 +1,33 @@
+package com.example.fpagnoux.monapplibd;
+
+public class Memory {
+
+    //déclare des listes et instancie les images des themes dans les listes
+    //BD
+    //Tintin
+    private int[] imgTintin = new int[] {R.drawable.t1_1, R.drawable.t1_2, R.drawable.t1_3, R.drawable.t1_4, R.drawable.t1_5, R.drawable.t1_6, R.drawable.t1_7, R.drawable.t1_8};
+    //Titeuf
+    //private int[] imgTiteuf = new int[] {R.drawable.t2_1, R.drawable.t2_2, R.drawable.t2_3, R.drawable.t2_4, R.drawable.t2_5, R.drawable.t2_6, R.drawable.t2_7, R.drawable.t2_8};
+    //Simson
+    //private int[] imgSimson = new int[] {R.drawable.s3_1, R.drawable.s3_2, R.drawable.s3_3, R.drawable.s3_4, R.drawable.s3_5, R.drawable.s3_6, R.drawable.s3_7, R.drawable.s3_8};
+    //Manga
+    //Naruto
+    //private int[] imgNaruto = new int[] {R.drawable.n4_1, R.drawable.n4_2, R.drawable.n4_3, R.drawable.n4_4, R.drawable.n4_5, R.drawable.n4_6, R.drawable.n4_7, R.drawable.n4_8};
+    //Fairy Tail
+    //private int[] imgFT = new int[] {R.drawable.f5_1, R.drawable.f5_2, R.drawable.f5_3, R.drawable.f5_4, R.drawable.f5_5, R.drawable.f5_6, R.drawable.f5_7, R.drawable.f5_8};
+    //One Piece
+    //private int[] imgOP = new int[] {R.drawable.o6_1, R.drawable.o6_2, R.drawable.o6_3, R.drawable.o6_4, R.drawable.o6_5, R.drawable.o6_6, R.drawable.o6_7, R.drawable.o6_8};
+    //Comics
+    //Super Man
+    //private int[] imgSuperM = new int[] {R.drawable.s7_1, R.drawable.s7_2, R.drawable.s7_3, R.drawable.s7_4, R.drawable.s7_5, R.drawable.s7_6, R.drawable.s7_7, R.drawable.s7_8};
+    //Spider Man
+    //private int[] imgSpiderM = new int[] {R.drawable.s8_1, R.drawable.s8_2, R.drawable.s8_3, R.drawable.s8_4, R.drawable.s8_5, R.drawable.s8_6, R.drawable.s8_7, R.drawable.s8_8};
+    //Avenger
+    //private int[] imgAvengers = new int[] {R.drawable.a9_1, R.drawable.a9_2, R.drawable.a9_3, R.drawable.a9_4, R.drawable.a9_5, R.drawable.a9_6, R.drawable.a9_7, R.drawable.a9_8};
+
+    //Propriétés
+    String theme;
+    String nomJoueur;
+    int nbCoups;
+
+}
