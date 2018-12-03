df0326aa4772783f9f5d2cb59df4c4a17f7a87d6
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-12-03 14:14:24 +0100

    drry

--- app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
@@ -18,7 +18,7 @@ public class activityClassement extends AppCompatActivity {
         String leTheme = this.getIntent().getExtras().getString("Theme");
 
         test = this.findViewById(R.id.viewPseudo);
-        test.setText(leTheme);
+        test.setText(leTheme); //
 
 
 
