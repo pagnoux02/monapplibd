5a25db8f8afa7753f304451938bafcb2b280a51c
francois pagnoux <francois.pagnoux@outlook.fr>
2018-12-03 16:08:35 +0200

    jpush a 16h08

--- app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
@@ -1,11 +1,13 @@
 package com.example.fpagnoux.monapplibd;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import java.util.Random;
 
@@ -17,11 +19,17 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
         //liste avec les 3 image
     private int[] imgdujeu = new int[] {android.R.drawable.sym_def_app_icon,android.R.drawable.star_big_off,android.R.drawable.star_big_on};
 
+
     private ImageView img1 ,img2 ,img3,img4,img5,img6,img7,img8,img9;
+
+  // private ImageView listimgaview[] = img1 ,img2 ,img3,img4,img5,img6,img7,img8,img9;
+
+    //   private List l = new LinkedList(img1,img2,img3,img4,img5,img6,img7,img8,img9);
     private TextView text;
     private int score = 0;
     private String theme;
     private Button start;
+    private Boolean jeu = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
         text = (TextView)findViewById(R.id.txtscore);
         start = (Button) findViewById(R.id.btnstart);
 
+       // text.setText(String.valueOf( listimgaview[1]));
         start.setOnClickListener(this);
 
         // instancie les images
@@ -56,6 +65,8 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
         img7.setOnClickListener(this);
         img8.setOnClickListener(this);
         img9.setOnClickListener(this);
+
+
     }
 
 
@@ -64,9 +75,16 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.btnstart :
+            case R.id.btnstart:
                 jouer(1);
                 break;
+        }
+
+                if (jeu == true ){
+
+
+                    switch (v.getId()) {
+
             case R.id.imgjeu1 :
                 score++;
                 text.setText(String.valueOf(score));
@@ -103,12 +121,51 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
             case R.id.imgjeu9 :
                 score=score+105;
                 text.setText(String.valueOf(score));
-                break;
+                break;}}
 
-        }}
+        }
+
      private void jouer (int theme) {
+         jeu = true;
+         int n = rand.nextInt(3);
+         int imgjeuplace = rand.nextInt(9);
 
-         img1.setImageResource(imgdujeu[n]);
+         switch (imgjeuplace) {
+             case 0:
+                 img1.setImageResource(imgdujeu[n]);
+                 break;
+             case 1:
+                 img2.setImageResource(imgdujeu[n]);
+                 break;
+             case 2:
+                 img3.setImageResource(imgdujeu[n]);
+                 break;
+             case 3:
+                 img4.setImageResource(imgdujeu[n]);
+                 break;
+             case 4:
+                 img5.setImageResource(imgdujeu[n]);
+                 break;
+             case 5:
+                 img6.setImageResource(imgdujeu[n]);
+                 break;
+             case 6:
+                 img7.setImageResource(imgdujeu[n]);
+                 break;
+             case 7:
+                 img8.setImageResource(imgdujeu[n]);
+                 break;
+             case 8:
+                 img9.setImageResource(imgdujeu[n]);
+                 break;
+
+         }
+
+
+
+
+
+
      }
 
 
