113d18e8d76fb63188e570c4feb855afc13e0465
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-12-06 16:10:01 +0100

    iguyuig

--- app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
@@ -8,16 +8,21 @@ import org.w3c.dom.Text;
 
 public class activityClassement extends AppCompatActivity {
     private TextView test;
+    private TextView afficher;
+    private collectionScores bdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classement);
-        
+
         String leTheme = this.getIntent().getExtras().getString("Theme");
 
         test = this.findViewById(R.id.viewPseudo);
-        test.setText(leTheme); 
+        test.setText(leTheme);
+
+        afficher = this.findViewById(R.id.affiche);
+        afficher.setText(bdd.nb_elements());
 
 
 
