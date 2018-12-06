6a42816e295db1f6a9c292936d35c7b881c72d64
francois pagnoux <francois.pagnoux@outlook.fr>
2018-12-06 13:33:12 +0200

    je push

--- app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/reneActivity.java
@@ -1,6 +1,8 @@
 package com.example.fpagnoux.monapplibd;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,19 +12,21 @@ import android.widget.TextView;
 
 
 import java.util.Random;
+import java.util.Timer;
 
-public class reneActivity  extends AppCompatActivity implements View.OnClickListener{
+public class reneActivity  extends AppCompatActivity implements View.OnClickListener {
     // creation d'un nombre aleatoir
     Random rand = new Random();
     int n = rand.nextInt(3);
 
-        //liste avec les 3 image
-    private int[] imgdujeu = new int[] {android.R.drawable.sym_def_app_icon,android.R.drawable.star_big_off,android.R.drawable.star_big_on};
+    //liste avec les 3 image ( pour l'aleatoir ) + 1 image de base
+    private int[] imgdujeu = new int[]{android.R.drawable.sym_def_app_icon, android.R.drawable.star_big_off, android.R.drawable.star_big_on, android.R.drawable.checkbox_off_background};
 
 
-    private ImageView img1 ,img2 ,img3,img4,img5,img6,img7,img8,img9;
+    private ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9;
 
-  // private ImageView listimgaview[] = img1 ,img2 ,img3,img4,img5,img6,img7,img8,img9;
+
+    // private ImageView listimgaview[] = img1 ,img2 ,img3,img4,img5,img6,img7,img8,img9;
 
     //   private List l = new LinkedList(img1,img2,img3,img4,img5,img6,img7,img8,img9);
     private TextView text;
@@ -36,10 +40,10 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rene_la_bd);
 
-        text = (TextView)findViewById(R.id.txtscore);
+        text = (TextView) findViewById(R.id.txtscore);
         start = (Button) findViewById(R.id.btnstart);
 
-       // text.setText(String.valueOf( listimgaview[1]));
+        // text.setText(String.valueOf( listimgaview[1]));
         start.setOnClickListener(this);
 
         // instancie les images
@@ -52,7 +56,6 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
         img7 = (ImageView) findViewById(R.id.imgjeu7);
         img8 = (ImageView) findViewById(R.id.imgjeu8);
         img9 = (ImageView) findViewById(R.id.imgjeu9);
-
 
 
         //ajoute un evenement qui verifie le click sur les img
@@ -76,100 +79,125 @@ public class reneActivity  extends AppCompatActivity implements View.OnClickList
 
         switch (v.getId()) {
             case R.id.btnstart:
+
                 jouer(1);
+
                 break;
         }
 
-                if (jeu == true ){
+        if (jeu == true) {
 
 
-                    switch (v.getId()) {
+            switch (v.getId()) {
 
-            case R.id.imgjeu1 :
-                score++;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu2:
-                score=score+15;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu3 :
-                score=score+30;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu4 :
-                score=score+30;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu5:
-                score=score+45;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu6 ://
-                score=score+60;
-                text.setText(String.valueOf(score));
-                break;
+                case R.id.imgjeu1:
+                    score++;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu2:
+                    score = score + 15;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu3:
+                    score = score + 30;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu4:
+                    score = score + 30;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu5:
+                    score = score + 45;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu6://
+                    score = score + 60;
+                    text.setText(String.valueOf(score));
+                    break;
 
-            case R.id.imgjeu7 :
-                score=score+75;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu8:
-                score=score+90;
-                text.setText(String.valueOf(score));
-                break;
-            case R.id.imgjeu9 :
-                score=score+105;
-                text.setText(String.valueOf(score));
-                break;}}
-
+                case R.id.imgjeu7:
+                    score = score + 75;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu8:
+                    score = score + 90;
+                    text.setText(String.valueOf(score));
+                    break;
+                case R.id.imgjeu9:
+                    score = score + 105;
+                    text.setText(String.valueOf(score));
+                    break;
+            }
         }
 
-     private void jouer (int theme) {
-         jeu = true;
-         int n = rand.nextInt(3);
-         int imgjeuplace = rand.nextInt(9);
+    }
 
-         switch (imgjeuplace) {
-             case 0:
-                 img1.setImageResource(imgdujeu[n]);
-                 break;
-             case 1:
-                 img2.setImageResource(imgdujeu[n]);
-                 break;
-             case 2:
-                 img3.setImageResource(imgdujeu[n]);
-                 break;
-             case 3:
-                 img4.setImageResource(imgdujeu[n]);
-                 break;
-             case 4:
-                 img5.setImageResource(imgdujeu[n]);
-                 break;
-             case 5:
-                 img6.setImageResource(imgdujeu[n]);
-                 break;
-             case 6:
-                 img7.setImageResource(imgdujeu[n]);
-                 break;
-             case 7:
-                 img8.setImageResource(imgdujeu[n]);
-                 break;
-             case 8:
-                 img9.setImageResource(imgdujeu[n]);
-                 break;
-
-         }
+    // Jouer peromet de generé 2 nbr aléatoir pour le choix de l'image et de la case de reception
+    private void jouer(int theme) {
+   
+        jeu = true;
+        int n = rand.nextInt(3);
+        int imgjeuplace = rand.nextInt(9);
 
 
+        switch (imgjeuplace) {
+
+            case 0:
+                personnagejouer(n, 3, 3, 3, 3, 3, 3, 3, 3);
+
+                break;
+            case 1:
+                personnagejouer(3, n, 3, 3, 3, 3, 3, 3, 3);
+                break;
+            case 2:
+                personnagejouer(3, 3, n, 3, 3, 3, 3, 3, 3);
+                break;
+            case 3:
+                personnagejouer(3, 3, 3, n, 3, 3, 3, 3, 3);
+                break;
+            case 4:
+                personnagejouer(3, 3, 3, 3, n, 3, 3, 3, 3);
+                break;
+            case 5:
+                personnagejouer(3, 3, 3, 3, 3, n, 3, 3, 3);
+                break;
+            case 6:
+                personnagejouer(3, 3, 3, 3, 3, 3, n, 3, 3);
+                break;
+            case 7:
+                personnagejouer(3, 3, 3, 3, 3, 3, 3, n, 3);
+                break;
+            case 8:
+                personnagejouer(3, 3, 3, 3, 3, 3, 3, 3, n);
+                break;
 
 
+        }
+    }
 
 
-     }
+    // affectation des image au differente case
+    private void personnagejouer(int monimg1, int monimg2, int monimg3, int monimg4, int monimg5, int monimg6, int monimg7, int monimg8, int monimg9) {
 
 
+        img1.setImageResource(imgdujeu[monimg1]);
+        img2.setImageResource(imgdujeu[monimg2]);
+        img8.setImageResource(imgdujeu[monimg3]);
 
+        img8.setImageResource(imgdujeu[monimg4]);
+        img8.setImageResource(imgdujeu[monimg5]);
+        img8.setImageResource(imgdujeu[monimg6]);
 
+        img8.setImageResource(imgdujeu[monimg7]);
+        img8.setImageResource(imgdujeu[monimg8]);
+        img9.setImageResource(imgdujeu[monimg9]);
+    }
 
 }
+
+
+
+
+
+
+
