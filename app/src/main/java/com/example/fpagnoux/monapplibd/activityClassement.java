3ed3cb340545302dd1dbf988025609164761bbeb
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-12-20 16:20:08 +0100

    yo

--- app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
+++ app/src/main/java/com/example/fpagnoux/monapplibd/activityClassement.java
@@ -3,26 +3,15 @@ package com.example.fpagnoux.monapplibd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-import static com.example.fpagnoux.monapplibd.R.layout.cell;
 
 public class activityClassement extends AppCompatActivity {
     private TextView test;
     private MySQLiteOpenHelper cmd;
+    private GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +19,18 @@ public class activityClassement extends AppCompatActivity {
         setContentView(R.layout.activity_classement);
 
         //String leTheme = this.getIntent().getExtras().getString("Theme");
-        GridView grid = (GridView)findViewById(R.id.grid_view);
-/*
-        int i = 0;
 
-        while(i<11){
-            for (rank unRank: cmd.getLesScores()
-                 ) {
-                //grid.setAdapter(); ;
-            }
-            i+=1;
-        }*/
+       /* GridView gridView = (GridView)findViewById(R.id.grid_view);
+        rankAdapter RankAdapter = new rankAdapter(this, convert(cmd.getLesScores()));
+        gridView.setAdapter(RankAdapter);*/
+    }
+    public rank[] convert(ArrayList<rank> uneListe) {
+        int i = 0;
+        rank[] desRanks = {};
+        while (i < 11) {
+            desRanks = uneListe.toArray(new rank[i]);
+            i += 1;
+        }
+        return desRanks;
     }
 }
